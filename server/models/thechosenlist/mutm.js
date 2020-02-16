4e19e2d6ed607891d1ff7e0bfb1d7be60d3f55e6
leoromerophd <leonardosuarezromero@gmail.com>
2020-02-15 19:56:18 -0600

    update

---
+++ server/models/thechosenlist/mutm.js
@@ -0,0 +1,61 @@
+// ### Import Libraries ##################################
+const mongoose = require('mongoose');
+//  Definir el Esquema de datos que se va a pedir 
+let Schema = mongoose.Schema;
+// Definir el Formato de la colección ~ Modelo
+
+let thechosenlistMUTMSchema = new Schema(
+    [{
+        symbol: String,
+        logo: {
+            url: String
+        },
+        nextEarnings: String,
+        CompanyInfo: {
+            symbol: String,
+            companyName: String,
+            exchange: String,
+            industry: String,
+            website: String,
+            description: String,
+            securityName: String,
+            issueType: String,
+            sector: String,
+            primarySicCode: String,
+            employees: String,
+            tags: Array,
+            address: String,
+            address2: String,
+            state: String,
+            city: String,
+            zip: String,
+            country: String,
+            phone: String
+        },
+        PriceTarget: {
+            symbol: String,
+            updatedDate: String,
+            priceTargetAverage: String,
+            priceTargetHigh: String,
+            priceTargetLow: String,
+            numberOfAnalysts: String,
+        },
+        SocialSentiment: {
+            sentiment: Number,
+            totalScores: Number,
+            positive: Number,
+            negative: Number
+        },
+        TheChosen: String,
+        HpFcast: String,
+        Scatters: String,
+        Frec20: String,
+        Frec50: String,
+        Frec200: String,
+    }]
+);
+
+//  crear el modelo para llegar a la colección 
+// el valor entre '' define la conección en plural 'etfinfos'
+let TheChosenList = mongoose.model('mutmthechosens', thechosenlistMUTMSchema);
+module.exports = TheChosenList;
---
+++ server/routes/thechosenlist/mutm.js
@@ -0,0 +1,37 @@
+//#####  Importar Liberías ###################
+const express = require('express');
+const bcrypt = require('bcrypt');
+const _ = require('underscore');
+const app = express();
+// ####### Import  Middlewares 
+const { verifyToken, verifyAdmin_Role } = require('../../middlewares/auth')
+    // # Import Schema Models #######################
+const TheChosenList = require('../../models/thechosenlist/mutm');
+
+// GET The Chosen MUTM list  
+app.get('/thechosenlist/mutm', (req, res) => {
+
+    let page = req.query.page || 0;
+    page = Number(page);
+    let limit = req.query.limit || 100;
+    limit = Number(limit);
+
+    TheChosenList.find({}, 'symbol logo url nextEarnings CompanyInfo symbol companyName exchange industry website description CEO securityName issueType sector primarySicCode employees tags address address2 state city zip country phone PriceTarget symbol updatedDate priceTargetAverage priceTargetHigh priceTargetLow numberOfAnalysts SocialSentiment TheChosen HpFcast Scatters Frec20 Frec50 Frec200 ')
+        .skip(page)
+        .limit(limit)
+        .exec((err, TheChosenList) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+            res.json({
+                ok: true,
+                TheChosenList
+            });
+        });
+});
+
+//  ### Exportar el app de Express ##############
+module.exports = app;
