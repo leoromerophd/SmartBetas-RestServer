01bd8af2ad9e2fb9a0380b30a65bce61cd2bcd18
leoromerophd <leonardosuarezromero@gmail.com>
2020-02-17 10:20:38 -0600

    update

--- server/routes/allcomplist/mutm.js
+++ server/routes/allcomplist/mutm.js
@@ -11,14 +11,7 @@ const AllCompList = require('../../models/allcomplist/mutm');
 // GET The Chosen mutm list  
 app.get('/allcomplist/mutm', (req, res) => {
 
-    let page = req.query.page || 0;
-    page = Number(page);
-    let limit = req.query.limit || 4;
-    limit = Number(limit);
-
     AllCompList.find({}, 'symbol logo url CompanyInfo companyName exchange industry website description CEO securityName issueType sector primarySicCode employees tags address address2 state city zip country phone PriceTarget HpFcast Scatters Frec20 Frec50 Frec200 AdjustedRsquared BetaofBenchmark SEofBenchmark tRatio Pvalue SEofRegression')
-        .skip(page)
-        .limit(limit)
         .exec((err, AllCompList) => {
             if (err) {
                 return res.status(400).json({
