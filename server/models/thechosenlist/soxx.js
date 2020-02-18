727250da330b6598b18d57ecc5895a00ea2e8465
leoromerophd <leonardosuarezromero@gmail.com>
2020-02-18 14:47:54 -0600

    update

--- server/models/thechosenlist/soxx.js
+++ server/models/thechosenlist/soxx.js
@@ -46,6 +46,9 @@ let thechosenlistSOXXSchema = new Schema(
             positive: Number,
             negative: Number
         },
+        AdjustedRsquared: String,
+        BetaofBenchmark: String,
+        SEofRegression: String,
         TheChosen: String,
         HpFcast: String,
         Scatters: String,
