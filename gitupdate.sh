a3720687bb1785237bcfc4c63a97e68b1e94f2be
leoromerophd <leonardosuarezromero@gmail.com>
2020-03-25 10:47:29 -0500

    update

--- gitupdate.sh
+++ gitupdate.sh
@@ -1,12 +1,15 @@
 ##!/bin/bash
 ### Actualiza los datos en git y Heroku
 
-
+EMAIL=$(leonardosuarezromero@gmail.com)
+PASWORD=$(Bolon&col4ever)
 git status
 git add .
 git commit -am "update"
 git push origin master
-heroku login
+heroku login -i
+$EMAIL
+$PASWORD
 git push heroku master
 heroku logs
 
