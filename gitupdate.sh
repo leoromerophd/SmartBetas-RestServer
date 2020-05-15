7a66020e429b39ad4cf76d2da60648ae3703a968
leoromerophd <leonardosuarezromero@gmail.com>
2020-05-15 12:00:22 -0500

    update

--- gitupdate.sh
+++ gitupdate.sh
@@ -5,6 +5,9 @@
 git status
 git add .
 git commit -am "update"
+git config --global http.postBuffer 500M
+git config --global http.maxRequestBuffer 100M
+git config --global core.compression 0
 git push origin master
 heroku login -i
 #heroku plugins:install heroku-repo
